a84abdf4e86874bb60bd66bbbad86549648b8a25
Ashley Creese <creese.ashley@gmail.com>
2022-02-11 19:35:43 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
+const observationRouter = require("./observations/observations.router")
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
+
+app.use("/observations", observationRouter);
 
 
 app.use(notFound);
